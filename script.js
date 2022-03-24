c4db498f84e14e8988f7761d86e38b5a432308e4
La-Nique <lanique.peterson02@myhunter.cuny.edu>
2022-03-24 18:24:07 -0400

    disable right clicks

--- script.js
+++ script.js
@@ -4,4 +4,8 @@
 // prints "hello world" in the browser's dev tools console
 console.log("hello world");
 
+// prints 2
 console.log(1+1);
+
+// DISABLE RIGHT CLICK.
+document.addEventListener('contextmenu', event => event.preventDefault());
